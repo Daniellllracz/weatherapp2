75577cd211b79045078643e903d9048fe4df420a
Daniellllracz <78079867+Daniellllracz@users.noreply.github.com>
2024-05-16 14:05:15 +0200

    Add project files.

---
+++ weatherapp/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace weatherapp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Class1> cities;
+        public MainWindow()
+        {
+            InitializeComponent();
+            cities = new List<Class1>();
+            var sr = new StreamReader("../../../src/cities.txt");
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream) cities.Add(new Class1(sr.ReadLine(),","));
+
+            varosList.ItemsSource = cities; 
+
+            
+        }
+
+    }
+}
---
+++ weatherapp/city.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+public class Class1
+{
+	public Class1() {
+	}
+	
+		public string name {  get; set; }
+		public double temperature { get; set; }
+		public int humidity { get; set; }
+		public double windSpeed { get; set; }
+
+
+		public Class1(string line, string separator)
+		{
+		string[] data = line.Split(separator);
+		name = data[0];
+		temperature = double.Parse(data[1].Replace('.',','));
+		humidity = int.Parse(data[2].Replace('.', ','));
+		windSpeed = double.Parse(data[3].Replace('.', ','));
+		}
+	
+
+    }
